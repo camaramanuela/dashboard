650079c8904e11d3db3466e7075a066c6a281968
Manuela Bognar <manuelabcamara@gmail.com>
2021-09-19 22:09:54 -0300

    dashboard page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ export function SideBar() {
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
         <Box>
-          <Text fontWeight="bold" color="gray.400" font-size="small">
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
             GERAL
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
@@ -21,7 +21,7 @@ export function SideBar() {
           </Stack>
         </Box>
         <Box>
-          <Text fontWeight="bold" color="gray.400" font-size="small">
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
             AUTOMAÇÃO
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,64 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { SideBar } from '../components/Sidebar';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enable: false,
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: { 
+      color: theme.colors.gray[600] 
+    },
+    axisTicks: { 
+      color: theme.colors.gray[600] 
+    },
+    categories: [
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z',
+      '2021-03-25T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  }
+};
+
+const series = [
+  { name: 'series1', data: [45, 168, 130, 150, 50, 42, 150] }
+];
+
 
 export default function Dashboard() {
   return(
@@ -8,6 +66,34 @@ export default function Dashboard() {
       <Header />
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <SideBar />
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          >
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+            <Chart
+              options={options}
+              series={series}
+              type="area"
+              height={160}
+            />
+          </Box>
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          >
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+            <Chart
+              options={options}
+              series={series}
+              type="heatmap"
+              height={160}
+            />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
