6ad215368ce10a5bec10e22a16af2a4a570713f5
Manuela Bognar <manuelabcamara@gmail.com>
2021-09-27 23:09:06 -0300

    page create user - layout

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,67 @@
+import { Box, Flex, Heading, Divider, HStack, VStack, SimpleGrid, Button} from "@chakra-ui/react"
+
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/Sidebar";
+import { Input } from "../../components/Form/Input";
+
+export default function CreateList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box 
+          flex="1" 
+          borderRadius={8} 
+          bg="gray.800" 
+          p="8"
+        >
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+          <Divider my="6" borderColor="gray.700"/>
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input 
+                name="name" 
+                label="Nome completo" 
+              />
+              <Input 
+                name="email" 
+                type="email" 
+                label="E-mail" 
+              />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input 
+                name="password" 
+                type="password" 
+                label="Senha" 
+              />
+              <Input
+                name="password_confirmation" 
+                type="password" 
+                label="Confirmação da senha" 
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button 
+                  type="submit" 
+                  colorScheme="pink"
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
