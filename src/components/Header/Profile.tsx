86822b031c3c4381db26037ebd8128dd9d9784b5
Manuela Bognar <manuelabcamara@gmail.com>
2021-09-28 20:04:54 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps) {
   return(
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Manuela Bognar</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          bognarmanuela42@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Manuela Bognar</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            bognarmanuela42@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar 
         size="md"
         name="Manuela Bognar"
