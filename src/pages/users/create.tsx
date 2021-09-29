aedb0dc29e4892266eeddea6ae56fb4563b76c18
Manuela Bognar <manuelabcamara@gmail.com>
2021-09-29 00:29:51 -0300

    responsive create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -16,14 +16,14 @@ export default function CreateList() {
           flex="1" 
           borderRadius={8} 
           bg="gray.800" 
-          p="8"
+          p={["6", "8"]}
         >
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700"/>
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input 
                 name="name" 
                 label="Nome completo" 
