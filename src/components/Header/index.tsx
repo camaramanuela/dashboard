92607f7f9682d296fa4f7c4c464febcbe376a588
Manuela Bognar <manuelabcamara@gmail.com>
2021-09-29 00:12:17 -0300

    responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, useBreakpointValue, Icon, IconButton } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SideBarDrawerContext';
 
 import { Logo } from './Logo';
 import { NotificationNav } from './NotificationsNav';
@@ -6,6 +8,8 @@ import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,18 @@ export function Header() {
       px="6"
       align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={< Icon as={RiMenuLine}/> }
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
       <Logo />
       { isWideVersion && <SearchBox />}
       <Flex
