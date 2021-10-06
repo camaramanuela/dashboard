aaac490ca99d2239fa8d160db0b5056ae2af6d7c
Manuela Bognar <manuelabcamara@gmail.com>
2021-10-05 23:36:18 -0300

    user create - form validation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,11 +1,42 @@
 import { Box, Flex, Heading, Divider, HStack, VStack, SimpleGrid, Button} from "@chakra-ui/react";
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatória'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null,
+    yup.ref('password')
+  ], 'As senhas precisam ser iguais'),
+}).required();
+
 export default function CreateList() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema)
+  });
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values, event) => {
+    console.log(values);
+
+    await new Promise(resolve => setTimeout(resolve, 2000))
+  }
+
   return (
     <Box>
       <Header />
@@ -14,10 +45,12 @@ export default function CreateList() {
         <SideBar />
 
         <Box 
+          as="form"
           flex="1" 
           borderRadius={8} 
           bg="gray.800" 
           p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
         >
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
@@ -28,11 +61,15 @@ export default function CreateList() {
               <Input 
                 name="name" 
                 label="Nome completo" 
+                {...register('name')}
+                error={errors.name}
               />
               <Input 
                 name="email" 
                 type="email" 
                 label="E-mail" 
+                {...register('email')}
+                error={errors.email}
               />
             </SimpleGrid>
 
@@ -41,11 +78,15 @@ export default function CreateList() {
                 name="password" 
                 type="password" 
                 label="Senha" 
+                {...register('password')}
+                error={errors.password}
               />
               <Input
                 name="password_confirmation" 
                 type="password" 
                 label="Confirmação da senha" 
+                {...register('password_confirmation')}
+                error={errors.password_confirmation}
               />
             </SimpleGrid>
           </VStack>
@@ -56,8 +97,9 @@ export default function CreateList() {
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
               <Button 
-                  type="submit" 
-                  colorScheme="pink"
+                type="submit" 
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
               >
                 Salvar
               </Button>
