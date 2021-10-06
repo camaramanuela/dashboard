010ca39717d43e769995d79b70d9d2f45e2f66a4
Manuela Bognar <manuelabcamara@gmail.com>
2021-10-06 02:17:25 -0300

    react-query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider, QueryClient } from 'react-query'
-
 import { SidebarDrawerProvider } from '../contexts/SideBarDrawerContext';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { theme } from '../styles/theme';
 import { makeServer } from '../services/mirage';
@@ -21,6 +21,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   ) 
 }
