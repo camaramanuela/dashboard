d487f9286f15e54bdab6347d931dfc4639742f45
Manuela Bognar <manuelabcamara@gmail.com>
2021-10-06 02:25:27 -0300

    fetching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,7 +8,7 @@ import { SideBar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
@@ -46,6 +46,8 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
                 Usuários
+
+                {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
             </Heading>
 
             <Link href="/users/create" passHref>
