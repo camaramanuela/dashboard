34b2c1681e3b053771ea1152a4088bf0152010ab
Manuela Bognar <manuelabcamara@gmail.com>
2021-10-07 05:04:16 -0300

    pagination logic

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -2,6 +2,9 @@ import {
   createServer,
   Factory,
   Model,
+  Server,
+  Response,
+  ActiveModelSerializer,
 } from 'miragejs'
 import faker from 'faker'
 
@@ -11,8 +14,11 @@ type User = {
   created_at: string
 }
 
-export function makeServer() {
+export function makeServer(): Server {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
     models: {
       user: Model.extend<Partial<User>>({}),
     },
@@ -25,20 +31,39 @@ export function makeServer() {
           return faker.internet.email().toLowerCase()
         },
         createdAt() {
-          return faker.date.recent(10)
+          return faker.date.recent(100)
         },
       }),
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 100)
     },
     
     routes() {
       this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response(
+          200, { 
+            'x-total-count': String(total) }, 
+            { users }
+          )
+      })
+
       this.post('/users')
 
       this.namespace = ''
